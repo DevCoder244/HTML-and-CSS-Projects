a58a65fd48d769436506513bf91629fdc434ecd2
DevCoder244 <ty5932074@gmail.com>
2025-08-01 19:38:53 -0400

    Add files via upload

---
+++ BAsic_JavaScript_1.js
@@ -0,0 +1,29 @@
+<!Doctype html>
+<html lang="en">
+<head>
+	<title>Basic_JavaScript_1</title>
+	<Meta charset="UTF-8>
+</head>
+<body>
+
+
+
+<script>
+
+	alert("Hello World! I am an alert box");
+</script>
+
+<script>
+
+	function My-First_Function () {
+	var string = "Kiss Me! I'm Irish";
+	var result = string.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+	}
+</script>
+
+
+
+</body>
+
+</html>
